2510272dd005678dc180b40ca20d780dc575710e
Carvalhodiego59 <diegocarvalho32059@gmail.com>
2023-09-29 01:30:37 -0300

    a

--- apiPastelaria.py
+++ apiPastelaria.py
@@ -2,5 +2,5 @@ from fastapi import FastAPI
 from settings import HOST, PORT, RELOAD
 app = FastAPI()
 if __name__ == "__main__":
-import uvicorn
-uvicorn.run('apiPastelaria:app', host=HOST, port=int(PORT), reload=RELOAD)
+    import uvicorn
+    uvicorn.run('apiPastelaria:app', host=HOST, port=int(PORT), reload=RELOAD)
